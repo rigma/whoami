017aeb3e0cd9b06a864380b81f3bde71ae925d63
rigma <rigbuntu@gmail.com>
2020-05-27 18:25:57 +0200

    feat: add signup action to security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,13 +1,51 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\Type\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Firebase\JWT\JWT;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/signup", name="signup", methods={"POST"})
+     */
+    public function signup(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = new User();
+
+        $body = $this->createForm(UserType::class, $user);
+        $body->submit($request->request->all());
+
+        if (!$body->isSubmitted()) {
+            return $this->json([
+                'error' => 'You have not submitted any data to the endpoint!',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        if (!$body->isValid()) {
+            return $this->json([
+                'error' => 'Invalid data submitted to the endpoint!',
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user = $body->getData();
+        $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'email' => $user->getEmail(),
+        ], Response::HTTP_CREATED);
+    }
+
     /**
      * @Route("/api/login", name="login", methods={"POST"})
      */
