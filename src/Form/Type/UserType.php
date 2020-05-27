a0c1d633df82e5da62367d5d1964ef95f641d644
rigma <rigbuntu@gmail.com>
2020-05-27 18:25:31 +0200

    feat: add user form validator

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -2,17 +2,24 @@
 namespace App\Form\Type;
 
 use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('csrf_protection', false);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email', TextType::class)
-            ->add('password', PasswordType::class)
-            ->add('phone_number', PhoneNumberType::class, ['default_region' => 'FR']);
+        $builder->add('email', TextType::class, ['required' => true])
+            ->add('password', PasswordType::class, ['required' => true])
+            ->add('phone_number', PhoneNumberType::class, ['required' => true, 'default_region' => 'FR']);
     }
 }
