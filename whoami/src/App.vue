48d45377c518f34a6e21296b7da2125d03ce02bf
rigma <rigbuntu@gmail.com>
2020-05-28 19:06:01 +0200

    feat: add home page
    fix: removing unused stylesheet

--- whoami/src/App.vue
+++ whoami/src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Accueil</router-link>
+      </li>
+      <li>
+        <router-link to="/connexion">Se connecter</router-link>
+      </li>
+      <li>
+        <router-link to="/s-inscrire">S'inscrire</router-link>
+      </li>
+    </ul>
+  </nav>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
--- whoami/src/router.js
+++ whoami/src/router.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: Home
+    }
+  ]
 })
