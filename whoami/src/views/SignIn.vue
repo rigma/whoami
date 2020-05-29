8993dc4a233304fedc55badc2c6801dfd1e2527c
rigma <rigbuntu@gmail.com>
2020-05-29 09:47:25 +0200

    feat: add sign in logic to view

--- whoami/src/views/SignIn.vue
+++ whoami/src/views/SignIn.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>Connexion</h1>
-  <form>
+  <form @submit.prevent="onSubmit">
     <div class="group">
       <label for="email">Adresse électronique :</label>
-      <input type="email"
+      <input v-model="email"
+        type="email"
         id="email"
         placeholder="mail@boite.dev"
         aria-placeholder="mail@boite.dev"
@@ -12,7 +13,8 @@
     </div>
     <div class="group">
       <label for="password">Votre mot de passe :</label>
-      <input type="password"
+      <input v-model="password"
+        type="password"
         id="password"
         placeholder="Il doit faire au moins 6 caractères"
         aria-placeholder="Il doit faire au moins 6 caractères"
@@ -24,15 +26,55 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { API_BASE_URL } from '../constants'
+import globalState from '../state'
 
-export default {
+export default defineComponent({
   name: 'SignIn',
   setup () {
     const email = ref(null)
     const password = ref(null)
+    const router = useRouter()
 
-    return { email, password }
+    const onSubmit = async () => {
+      if (globalState.token !== null) {
+        console.error('Already logged in!')
+        return
+      }
+
+      const res = await fetch(`${API_BASE_URL}/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email.value,
+          password: password.value
+        })
+      })
+
+      if (res.status >= 400) {
+        console.error('Wrong credentials!')
+        return
+      }
+
+      const token = await res.text()
+      globalState.token = token
+      globalState.sessionExpired = false
+
+      router.push('/')
+    }
+
+    return { email, password, onSubmit }
+  },
+  beforeRouteEnter (to, from, next) {
+    if (globalState.token !== null) {
+      return next('/')
+    }
+
+    next()
   }
-}
+})
 </script>
