a4b8dc53600fa6ac5c9b592fc232b576c692f431
rigma <rigbuntu@gmail.com>
2020-05-29 15:18:43 +0200

    feat: add constant warning messages

--- whoami/src/constants.js
+++ whoami/src/constants.js
@@ -1,1 +1,10 @@
 export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8001'
+export const WARNINGS = [
+  `Si j'étais vous, je ne toucherez pas à ce lien secret`,
+  'Ne touchez pas à ce lien !',
+  `S'il vous plaît, ne touchez pas à ce lien`,
+  `En quelle langue faut-il vous l'écrire : ne touchez pas à ce lien secret !`,
+  `Suffit ! Vous commencez à m'ennuyer : ne touchez plus à ce lien secret !`,
+  `L'Union Galarienne réitère sa demande : ne touchez plus au lien secret !`,
+  `Et zut ! Cliquez donc sur ce lien secret si vous voulez : vous n'avez pas les droits d'admin de toutes façons !`
+]
