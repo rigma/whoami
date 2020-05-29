241aa67e21871b920347e9da71630b705caec9d2
rigma <rigbuntu@gmail.com>
2020-05-29 10:21:02 +0200

    feat: add logout logic

--- whoami/src/App.vue
+++ whoami/src/App.vue
@@ -4,6 +4,8 @@
       <li>
         <router-link to="/">Accueil</router-link>
       </li>
+    </ul>
+    <ul v-if="!loggedIn">
       <li>
         <router-link to="/connexion">Se connecter</router-link>
       </li>
@@ -11,6 +13,9 @@
         <router-link to="/s-inscrire">S'inscrire</router-link>
       </li>
     </ul>
+    <div v-else>
+      <a @click.prevent="onLogOut" href="javascript://">Se déconnecter</a>
+    </div>
   </nav>
   <main>
     <router-view />
@@ -18,7 +23,19 @@
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+import { defineComponent, computed } from 'vue'
+import globalState from './state'
+
+export default defineComponent({
+  name: 'App',
+  setup () {
+    const loggedIn = computed(() => globalState.token !== null)
+    
+    const onLogOut = () => {
+      globalState.token = null
+    }
+
+    return { loggedIn, onLogOut }
+  }
+})
 </script>
